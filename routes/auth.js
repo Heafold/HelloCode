7a65dc42025ef5a99ebfe9abbf3a125aec2b72cf
Heafold <mcorentin62@gmail.com>
2023-09-05 10:28:52 +0200

    fix-auth-add-admin-age

--- routes/auth.js
+++ routes/auth.js
@@ -1,63 +1,82 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 const router = express.Router();
 
 function checkAuthenticated(req, res, next) {
-    if (req.session.user) {
-        return next();
-    }
-    res.redirect("/login");
+  if (req.session.user) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkAdmin(req, res, next) {
+  if (req.session.adminAuthenticated) {
+    return next();
+  }
+  res.redirect("/admin-login");
 }
 
 router.get("/", checkAuthenticated, (req, res) => {
-    res.render("index", { username: req.session.user.username });
+  res.render("index", { username: req.session.user.username });
 });
 
-router.get('/register', (req, res) => {
-    res.render('register');
+router.get("/register", (req, res) => {
+  res.render("register");
 });
 
-router.post('/register', async (req, res) => {
-    try {
-        if (req.body.password !== req.body.passwordConfirm) {
-            throw new Error('Les mots de passe ne correspondent pas');
-        }
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-        const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        });
-
-        await user.save();
-        req.session.user = user;
-        res.redirect('/');
-    } catch (error) {
-        res.redirect('/register');
+router.post("/register", async (req, res) => {
+  try {
+    if (req.body.password !== req.body.passwordConfirm) {
+      throw new Error("Les mots de passe ne correspondent pas");
     }
+
+    const user = new User(req.body);
+
+    await user.save();
+    req.session.user = user;
+    res.redirect("/");
+  } catch (error) {
+    res.redirect("/register");
+  }
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.post('/login', async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    if (user && await bcrypt.compare(req.body.password, user.password)) {
-        req.session.user = user;
-        res.redirect('/');
-    } else {
-        res.redirect('/login');
-    }
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+    req.session.user = user;
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
 });
 
-router.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/login');
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+
+router.get("/admin-login", (req, res) => {
+  res.render("admin-login");
+});
+
+router.post("/admin-login", (req, res) => {
+  const adminPassword = process.env.ADMIN_PASSWORD;
+  if (req.body.password === adminPassword) {
+    req.session.adminAuthenticated = true;
+    res.redirect("/admin");
+  } else {
+    res.redirect("/admin-login");
+  }
+});
+
+router.get("/admin", checkAdmin, (req, res) => {
+  res.render("admin");
 });
 
 module.exports = router;
